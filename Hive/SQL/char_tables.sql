39f95c08f279ea34675b4a836f8cee8487d607aa
Rajko Stojadinovic <admin@rajko.info>
2012-11-27 14:01:01 +0100

    Remove SQF folder (as it's functionality is now in mainline) Update SQL Tables to more closely match Public Hive layout

--- Hive/SQL/char_tables.sql
+++ Hive/SQL/char_tables.sql
@@ -4,38 +4,26 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for `Player_DATA`
 -- ----------------------------
 CREATE TABLE `Player_DATA` (
-  `PlayerUID` varchar(20) NOT NULL DEFAULT '0',
-  `PlayerName` varchar(128) CHARACTER SET utf8 NOT NULL DEFAULT 'Null',
+  `PlayerUID` varchar(32) NOT NULL,
+  `PlayerName` varchar(128) CHARACTER SET utf8 NOT NULL,
   `PlayerMorality` int(11) NOT NULL DEFAULT '0',
   `PlayerSex` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
   PRIMARY KEY (`PlayerUID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
-
--- ----------------------------
--- Table structure for `Player_LOGIN`
--- ----------------------------
-CREATE TABLE `Player_LOGIN` (
-  `LoginID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `PlayerUID` varchar(20) NOT NULL,
-  `CharacterID` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `Datestamp` datetime NOT NULL,
-  `Action` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`LoginID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for `Character_DATA`
 -- ----------------------------
 CREATE TABLE `Character_DATA` (
   `CharacterID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `PlayerUID` varchar(20) NOT NULL DEFAULT '0',
-  `InstanceID` int(11) NOT NULL DEFAULT '0',
+  `PlayerUID` varchar(32) NOT NULL,
+  `InstanceID` int(11) NOT NULL,
   `Datestamp` datetime DEFAULT NULL,
   `LastLogin` datetime NOT NULL,
-  `Inventory` longtext,
-  `Backpack` longtext,
+  `Inventory` text,
+  `Backpack` text,
   `Worldspace` varchar(128) NOT NULL DEFAULT '[]',
-  `Medical` varchar(300) NOT NULL DEFAULT '[]',
+  `Medical` varchar(256) NOT NULL DEFAULT '[]',
   `Alive` tinyint(3) UNSIGNED NOT NULL DEFAULT '1',
   `Generation` int(11) UNSIGNED NOT NULL DEFAULT '1',
   `LastAte` datetime NOT NULL,
@@ -43,13 +31,24 @@ CREATE TABLE `Character_DATA` (
   `KillsZ` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `HeadshotsZ` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `DistanceFoot` int(11) NOT NULL DEFAULT '0',
-  `Duration` int(11) NOT NULL DEFAULT '0',
-  `CurrentState` varchar(200) NOT NULL DEFAULT '[]',
+  `Duration` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  `CurrentState` varchar(128) NOT NULL DEFAULT '[]',
   `KillsH` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `Model` varchar(50) NOT NULL DEFAULT '"Survivor2_DZ"',
+  `Model` varchar(64) NOT NULL DEFAULT '"Survivor2_DZ"',
   `KillsB` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `Humanity` int(11) NOT NULL DEFAULT '2500',
   PRIMARY KEY (`CharacterID`),
-  KEY `PlayerUID` (`PlayerUID`) USING BTREE,
-  KEY `Alive` (`Alive`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+  KEY `CharFetch` (`PlayerUID`,`Alive`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for `Player_LOGIN`
+-- ----------------------------
+CREATE TABLE `Player_LOGIN` (
+  `LoginID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `PlayerUID` varchar(32) NOT NULL,
+  `CharacterID` int(11) UNSIGNED NOT NULL,
+  `Datestamp` datetime NOT NULL,
+  `Action` tinyint(3) NOT NULL,
+  PRIMARY KEY (`LoginID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
--- Hive/SQL/obj_tables.sql
+++ Hive/SQL/obj_tables.sql
@@ -3,19 +3,19 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for `Object_DATA`
 -- ----------------------------
-DROP TABLE IF EXISTS `Object_DATA`;
 CREATE TABLE `Object_DATA` (
   `ObjectID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `ObjectUID` bigint(20) NOT NULL DEFAULT '0',
-  `Instance` int(11) UNSIGNED NOT NULL,
-  `Classname` varchar(50) DEFAULT NULL,
+  `Instance` int(11) NOT NULL,
+  `Classname` varchar(64) DEFAULT NULL,
   `Datestamp` datetime NOT NULL,
   `CharacterID` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `Worldspace` varchar(128) NOT NULL DEFAULT '[]',
-  `Inventory` longtext,
+  `Inventory` text,
   `Hitpoints` varchar(512) NOT NULL DEFAULT '[]',
   `Fuel` double(13,5) NOT NULL DEFAULT '1.00000',
   `Damage` double(13,5) NOT NULL DEFAULT '0.00000',
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`ObjectID`),
   KEY `ObjectUID` (`ObjectUID`),
   KEY `Instance` (`Instance`)
